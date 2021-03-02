949c1b432869f80e81e8a1419ce46fba98d36b06
drdobbins <sdobbins75@gmail.com>
2021-03-01 21:15:47 -0600

    fixed mileage updater

--- odometer.py
+++ odometer.py
@@ -8,12 +8,12 @@ def increase_mileage():
 	current_attempts = data["attempts"] #extract the current mileage
 	data["attempts"] = current_attempts + 1 #increment the count
 
-	write_file = open('.mileage.json','w')
+	write_file = open('/home/pi/mileage.json','w')
 	json.dump(data,write_file)
 	write_file.close()
 	
 def read_mileage():
-	read_file = open('.mileage.json', 'r') #open the json file
+	read_file = open('/home/pi/mileage.json', 'r') #open the json file
 	data = json.load(read_file) #read in the data
 	read_file.close() #close the json file. 
 
